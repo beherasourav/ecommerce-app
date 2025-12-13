4c54e04097434269d0f2c130f98f407040f7c0bc
Sourav Behera <souravbehera3276@gmail.com>
2025-12-13 21:56:35 +0530

    Add Login page

---
+++ src/Components/Bredcrums/BredCrums.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Bredcrums.css" 
+import arrow_icon from "../../assets/arrow_icon.png"
+const Bredcrums = (props) => {
+  return (
+    <div className='bredcrums'>
+     Home    <img src={arrow_icon} alt="" height="30px"/> SHOP <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Bredcrums
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-
-const Product = () => {
+import React, { useContext } from 'react'
+import {ShopContext} from "../Context/ShopContext"
+import { useParams } from 'react-router-dom';
+const Product = () => { 
+  const {all_product} = useContext(ShopContext); 
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === productId)
   return (
     <div>Product</div>
   )
