4d08da98ed2b1f8e6f0b3a1c2c047c8c637903c6
Sourav Behera <souravbehera3276@gmail.com>
2025-12-22 22:23:37 +0530

    Add Some code

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -26,7 +26,35 @@ const ProductDisplay = (props) => {
                 <img src={star_icon} alt="" height="20px" /> 
                 <img src={star_dull_icon} alt="" height="20px"/> 
                 <p>(130)</p>
-            </div> 
+            </div>  
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-prices-old">
+                    ${product.old_price}
+                </div>
+                <div className="productdisplay-right-prices-new">
+                    ${product.new_price}
+                </div> 
+                <div className="productdisplay-right-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, inventore ab itaque nobis voluptatibus, cupiditate repellat aperiam eveniet commodi quis, sequi totam unde corporis. Eius alias voluptatibus corrupti recusandae laborum.
+                </div> 
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-size">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div> 
+                    <button>ADD TO CART</button>
+                    <div className="productdisplay-right-category">
+                        <span>Category:<span>Women, T-Shirt, Crop Top</span></span>
+                    </div>
+                     <div className="productdisplay-right-category">
+                        <span>Tags:<span>Modern, Latest, Trend Shorts</span></span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
