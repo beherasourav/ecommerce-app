1c5ea0c2fc9f4575d869fb7160390555fc10bae5
Sourav Behera <souravbehera3276@gmail.com>
2025-12-24 13:55:29 +0530

    Add some code in relatedproducts

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Bredcrums from '../Components/Bredcrums/BredCrums';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import Description from '../Components/Description/Description';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 const Product = () => {
   const {all_product} = useContext(ShopContext);
   const {productId} = useParams();
@@ -12,7 +13,8 @@ const Product = () => {
     <div>
       <Bredcrums product={product}/>
       <ProductDisplay product={product}/>
-      <Description />
+      <Description /> 
+      <RelatedProducts />
     </div>
   )
 }
